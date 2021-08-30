215f43b97577e01431837931ef8f2c21c2f7c7bd
afinney1 <amfinney792@gmail.com>
2021-08-30 15:48:00 -0500

    got list of lat, lng from gmaps.directions

--- TCapp.py
+++ TCapp.py
@@ -7,20 +7,30 @@ from PIL import Image
 
 
 def main():
+    #Get user's starting point and end point
     st.title("TravelCast" )
     start = st.text_input("Enter starting city: ", "Jackson, MS, USA")
     end = st.text_input("Enter destination city: ", "Memphis, TN, USA")
-    #owm ->lat lon-> gmp - directions> TCapp.py
-    #owm -weather -> gmp - directions> TCapp.py
+
+
+    #forecast initiation
     forecast = owm()
     start_coordinates = (forecast.lat, forecast.lon)
     end_coordinates = (forecast.lat, forecast.lon)
+
+    #route initiation
     route = GMProute()
     route.start = start
     route.end = end
     directions = route.my_map()
+    path = route.inter_locations()
+    #print(path)
+    d = route.directions
+    st.write(d)
     #print(directions)
     st.image(directions)
+    for doc in d[0]["legs"][0]["steps"]:
+        print(doc["end_location"])
     return None
 
    
--- gmp_interface.py
+++ gmp_interface.py
@@ -18,11 +18,22 @@ class GMProute:
     end: str = "Memphis,MS,USA"
     gmaps = googlemaps.Client(key=key)
     directions = gmaps.directions(start, end)
+
     def my_map(self):
         key = self.key
         start = self.start
+        path = self.directions
         map_url = f"https://maps.googleapis.com/maps/api/staticmap?center={start}&size=720x720&zoom=13&key={key}"
         response = requests.get(map_url).content
         return response
+
+    def inter_locations(self):
+        """
+        Return a list of locations between the start and end locations
+        """
+        start = self.start
+        end = self.end
+        distance_matrix = self.gmaps.distance_matrix(self.start, self.end)
+        return distance_matrix
     #start_gmaps.Geocoding(address=start)
      
